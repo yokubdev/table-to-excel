2cb433804c975abd2ac14a25fd20df2f0e91dbf4
yokubdev <161561531+yokubdev@users.noreply.github.com>
2024-11-01 21:21:28 +0500

    feat: initial commit

--- src/components/TableToExcel/TableToExcel.tsx
+++ src/components/TableToExcel/TableToExcel.tsx
@@ -59,6 +59,7 @@ const TableToExcel: React.FC<IProps> = ({ headers, dataRows, name }) => {
             });
         });
 
+
         const workbook = XLSX.utils.book_new();
         XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
 
